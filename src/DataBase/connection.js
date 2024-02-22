3dbe47febb8966946e1f965f77e1d2335f4be8d2
MateoGomez11 <mgomez98611@umanizales.edu.co>
2024-02-22 15:47:40 -0500

    config new project

---
+++ src/DataBase/connection.js
@@ -0,0 +1,13 @@
+const { host } = require('pg/lib/defaults');
+const { Sequelize } = require('sequelize');
+
+var dataBase = 'restaurantDB';
+var userName = 'postgres';
+var password = '123456789';
+
+const connection = new Sequelize(dataBase, userName, password, {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+module.exports = connection;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const connection = require('./DataBase.connection');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 1337;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+connection.sync({force: false})
+    .then(() => { 
+        console.log('Base de datos sincronizada'); 
+        app.listen(port, () => { 
+            console.log("La aplicación está corriendo en el puerto: " + port);
+        }) 
+    })
+    .catch((error) => { 
+        console.error('Error al sincronizar la base de datos:', error); 
+}); 
