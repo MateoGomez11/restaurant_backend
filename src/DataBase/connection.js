e032bc12a3c3103e35e9d643ccdc381125b11297
Mateo Gomez <mgomez98611@umanizales.edu.co>
2024-03-09 18:26:19 -0500

    cambio contrasena postgress

--- src/DataBase/connection.js
+++ src/DataBase/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 var dataBase = 'restaurantDB';
 var userName = 'postgres';
-var password = '1105';
+var password = '123456789';
 
 const connection = new Sequelize(dataBase, userName, password, {
     host: 'localhost',
